bc493e627c480a6fc5fa7a64c8793ee28f96765b
sofiankerkhof <sofian.kerkhof@skyline.be>
2025-09-23 11:25:01 +0200

    Added function that will be implemented later, it will be used in all QActions. Therefore QA1 is used as basis for precompiled code.

---
+++ QAction_1/HTTP/StatusCode.cs
@@ -0,0 +1,16 @@
+﻿namespace Skyline.DataMiner.Scripting.HTTP
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class StatusCode
+    {
+        public static bool CheckStatusCode()
+        {
+            return true;
+        }
+    }
+}
