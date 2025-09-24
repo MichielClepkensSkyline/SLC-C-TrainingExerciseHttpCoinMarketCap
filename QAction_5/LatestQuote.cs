2e7b2eb9ca8bd0ce9d94fc1319b07351706ee1f7
TajanaSA <tajana.salkic@skyline.be>
2025-09-24 09:06:58 +0200

    Added parsing for the Latest Quote in QAction_5

--- QAction_5/LatestQuote.cs
+++ QAction_5/LatestQuote.cs
@@ -1,13 +1,6 @@
-﻿
-
-namespace QAction_5
+﻿namespace QAction_5
 {
 	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Net.NetworkInformation;
-	using System.Text;
-	using System.Threading.Tasks;
 	using Newtonsoft.Json;
 
 	public class Data
--- QAction_5/QAction_5.cs
+++ QAction_5/QAction_5.cs
@@ -1,9 +1,14 @@
+using QAction_5;
+
+using Skyline.DataMiner.Scripting;
+using Skyline.DataMiner.Utils.Protocol.Extension;
+using Skyline.DataMiner.Utils.SecureCoding.SecureSerialization.Json.Newtonsoft;
+
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
-using Skyline.DataMiner.Scripting;
-using Skyline.DataMiner.Utils.SecureCoding.SecureSerialization.Json.Newtonsoft;
 
 /// <summary>
 /// DataMiner QAction Class: Parse Latest Quote.
@@ -18,11 +23,44 @@ public static class QAction
 	{
 		try
 		{
-			
+			string json = protocol.GetParameter(Parameter.responselatestquote_10).ToString();
+			LatestQuote latestQuote = SecureNewtonsoftDeserialization.DeserializeObject<LatestQuote>(json);
+			string statusCode = protocol.GetParameter(Parameter.statuscodelatestquote_9).ToString();
+			protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|statusCode: '{statusCode}'", LogType.Information, LogLevel.NoLogging);
+
+			int status = Int32.Parse(statusCode.Split(' ')[1]);
+
+			if (status == 200)
+			{
+				FillLatestQuoteParameters(protocol, latestQuote.Data);
+			}
+			else
+			{
+
+			}
 		}
 		catch (Exception ex)
 		{
 			protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Exception thrown:{Environment.NewLine}{ex}", LogType.Error, LogLevel.NoLogging);
 		}
+	}
+
+	private static void FillLatestQuoteParameters(SLProtocol protocol, Data latestQuote)
+	{
+		Dictionary<int, object> parameters = new Dictionary<int, object>
+		{
+			{ Parameter.latestquoteactivecryptocurrencies_30, latestQuote.ActiveCryptocurrencies},
+			{ Parameter.latestquotetotalcryptocurrencies_31, latestQuote.TotalCryptocurrencies },
+			{ Parameter.latestquoteactiveexchanges_32, latestQuote.ActiveExchanges },
+			{ Parameter.latestquotetotalexchanges_33, latestQuote.TotalExchanges },
+			{ Parameter.latestquoteethdominance_34, latestQuote.EthDominance },
+			{ Parameter.latestquotebtcdominance_35, latestQuote.BtcDominance },
+			{ Parameter.latestquoteethdominance24percentagechange_36, latestQuote.EthDominance24hPercentageChange },
+			{ Parameter.latestquotebtcdominance24percentagechange_37, latestQuote.BtcDominance24hPercentageChange },
+			{ Parameter.latestquotedefi24hpercentagechange_38, latestQuote.Defi24hPercentageChange },
+			{ Parameter.latestquotetodaychangepercent_39, latestQuote.TodayChangePercent },
+			{ Parameter.latestquotelastupdate_40, latestQuote.LastUpdated },
+		};
+		protocol.SetParameters(parameters);
 	}
 }
