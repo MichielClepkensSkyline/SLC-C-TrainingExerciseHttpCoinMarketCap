5df993e972381b47a02f1f1b805fe9a13416ce44
TajanaSA <tajana.salkic@skyline.be>
2025-10-01 13:41:17 +0200

    Added unit testing for QAction_3, and added a return if ID is null for LatestListing

--- QAction_3/QAction_3.cs
+++ QAction_3/QAction_3.cs
@@ -50,31 +50,30 @@ public static class QAction
 
 			foreach (Listing latest_listing in latestListings.Listings)
 			{
-				if (!String.IsNullOrWhiteSpace(latest_listing.Id))
-				{
-					latestListingsTableContent[latest_listing.Id] = new LatestlistingsQActionRow
-					{
-						Latestlistingsid_101 = latest_listing.Id.ToString(),
-						Latestlistingsname_102 = latest_listing.Name ?? exceptionValue.ToString(),
-						Latestlistingssymbol_103 = latest_listing.Symbol ?? exceptionValue.ToString(),
-						Latestlistingsdateadded_104 = latest_listing.DateAdded.ToOADate(),
-						Latestlistingscirculatingsupply_105 = latest_listing.TotalSupply,
-						Latestlistingsrank_106 = latest_listing.CmcRank ?? exceptionValue,
-						Latestlistingslastupdated_107 = latest_listing.LastUpdated.ToOADate(),
-						Latestlistingsquoteprice_108 = latest_listing.Quote.USD.Price ?? exceptionValue,
-						Latestlistings1hpercentagechange_109 = latest_listing.Quote.USD.PercentChange1h ?? exceptionValue,
-						Latestlistingsquotevolumechange24h_110 = latest_listing.Quote.USD.VolumeChange24h ?? exceptionValue,
-						Latestlistingsquotemarketcap_111 = latest_listing.Quote.USD.MarketCap ?? exceptionValue,
-						Latestlistingsplatformname_112 = latest_listing.Platform?.Name ?? exceptionValue.ToString(),
-						Latestlistingsmaxsupply_114 = latest_listing.MaxSupply ?? exceptionValue,
-						Latestlistingsquotemarketcapdominance_115 = latest_listing.Quote.USD.MarketCapDominance ?? exceptionValue,
-						Latestlistingsquotevolume24h_116 = latest_listing.Quote.USD.Volume24h ?? exceptionValue,
-					}.ToObjectArray();
-				}
-				else
+				if (String.IsNullOrWhiteSpace(latest_listing.Id))
 				{
 					protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run| Latest Listing Id is null", LogType.Error, LogLevel.NoLogging);
+					return;
 				}
+
+				latestListingsTableContent[latest_listing.Id] = new LatestlistingsQActionRow
+				{
+					Latestlistingsid_101 = latest_listing.Id.ToString(),
+					Latestlistingsname_102 = latest_listing.Name ?? exceptionValue.ToString(),
+					Latestlistingssymbol_103 = latest_listing.Symbol ?? exceptionValue.ToString(),
+					Latestlistingsdateadded_104 = latest_listing.DateAdded.ToOADate(),
+					Latestlistingscirculatingsupply_105 = latest_listing.TotalSupply,
+					Latestlistingsrank_106 = latest_listing.CmcRank ?? exceptionValue,
+					Latestlistingslastupdated_107 = latest_listing.LastUpdated.ToOADate(),
+					Latestlistingsquoteprice_108 = latest_listing.Quote.USD.Price ?? exceptionValue,
+					Latestlistings1hpercentagechange_109 = latest_listing.Quote.USD.PercentChange1h ?? exceptionValue,
+					Latestlistingsquotevolumechange24h_110 = latest_listing.Quote.USD.VolumeChange24h ?? exceptionValue,
+					Latestlistingsquotemarketcap_111 = latest_listing.Quote.USD.MarketCap ?? exceptionValue,
+					Latestlistingsplatformname_112 = latest_listing.Platform?.Name ?? exceptionValue.ToString(),
+					Latestlistingsmaxsupply_114 = latest_listing.MaxSupply ?? exceptionValue,
+					Latestlistingsquotemarketcapdominance_115 = latest_listing.Quote.USD.MarketCapDominance ?? exceptionValue,
+					Latestlistingsquotevolume24h_116 = latest_listing.Quote.USD.Volume24h ?? exceptionValue,
+				}.ToObjectArray();
 			}
 
 			protocol.FillArray(Parameter.Latestlistings.tablePid, latestListingsTableContent.Values.ToList(), NotifyProtocol.SaveOption.Full);
---
+++ QAction_3Tests/QActionTests.cs
@@ -0,0 +1,189 @@
+﻿namespace Tests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	using Moq;
+
+	using Skyline.DataMiner.Scripting;
+	using Skyline.DataMiner.Scripting.LatestListings;
+	using Skyline.DataMiner.Utils.SecureCoding.SecureSerialization.Json.Newtonsoft;
+
+	[TestClass]
+	public class QActionTests
+	{
+		private static Mock<SLProtocol> protocol;
+
+		[TestInitialize]
+
+		public void TestInitialize()
+		{
+			protocol = new Mock<SLProtocol>();
+		}
+
+		[TestMethod]
+		public void RunTest_ShouldReturn_StatusCheckFails()
+		{
+			//Arrange
+			protocol.Setup(p => p.GetParameter(Parameter.statuscodelatestlistings_3)).Returns("HTTP/1.1 500 Internal Server Error");
+
+			//Act
+
+			QAction.Run(protocol.Object);
+
+			//Assert
+
+			protocol.Verify(p => p.GetParameter(Parameter.responselatestlistings_4), Times.Never);
+			protocol.Verify(p => p.FillArray(It.IsAny<int>(), It.IsAny<object[]>()), Times.Never);
+		}
+
+		[TestMethod]
+		public void RunTest_ShouldReturn_ErrorCheckFails()
+		{
+			//Arrange
+			protocol.Setup(p => p.GetParameter(Parameter.statuscodelatestlistings_3)).Returns("HTTP/1.1 500 Internal Server Error");
+
+			var latestListing = new LatestListings
+			{
+				Status = new Status
+				{
+					ErrorCode = 1,
+					ErrorMessage = "Error message",
+				},
+			};
+
+			string json = Newtonsoft.Json.JsonConvert.SerializeObject(latestListing);
+
+			protocol.Setup(p => p.GetParameter(Parameter.responselatestlistings_4)).Returns(json);
+
+			//Act
+
+			QAction.Run(protocol.Object);
+
+			//Assert
+
+			protocol.Verify(p => p.FillArray(It.IsAny<int>(), It.IsAny<object[]>()), Times.Never);
+		}
+
+		[TestMethod]
+		public void RunTest_ShouldFillAray_ValidInputs()
+		{
+			//Arrange
+			protocol.Setup(p => p.GetParameter(Parameter.statuscodelatestlistings_3)).Returns("HTTP/1.1 200 OK");
+
+			var latestListing = new LatestListings
+			{
+				Status = new Status
+				{
+					ErrorCode = 0,
+					ErrorMessage = null,
+				},
+				Listings = new List<Listing>
+				{
+					new Listing
+					{
+						Id = "1",
+						Name = "Bitcoin",
+						Symbol = "BTC",
+						DateAdded = DateTime.Parse("2010-07-13T00:00:00Z"),
+						CirculatingSupply = 19864978,
+						MaxSupply = 21000000,
+						CmcRank = 1,
+						Platform = new Platform
+									{
+										Name = "Bitcoin",
+									},
+						LastUpdated = DateTime.Parse("2025-10-01T07:37:21.5113403Z"),
+						Quote = new Quote
+								{
+									USD = new USD
+									{
+									Price = (double?)101815.90673154943,
+									Volume24h = (double?)45620279708.066856,
+									VolumeChange24h = (double?)-7.5884,
+									PercentChange1h = (double?)-0.4304306,
+									PercentChange24h = (double?)-1.83894047,
+									MarketCap = (double?)2022570747272.2815,
+									MarketCapDominance = (double?)61.8788,
+									},
+								},
+					},
+				},
+			};
+
+			string json = Newtonsoft.Json.JsonConvert.SerializeObject(latestListing);
+
+			protocol.Setup(p => p.GetParameter(Parameter.responselatestlistings_4)).Returns(json);
+
+			//Act
+
+			QAction.Run(protocol.Object);
+
+			//Assert
+
+			protocol.Verify(p => p.Log(It.IsAny<string>(), It.IsAny<LogType>(), It.IsAny<LogLevel>()), Times.Never);
+			protocol.Verify(p => p.FillArray(It.IsAny<int>(), It.IsAny<List<object[]>>(), NotifyProtocol.SaveOption.Full), Times.Once);
+		}
+
+		[TestMethod]
+		public void RunTest_ShouldFillAray_InvalidInputIdNull()
+		{
+			//Arrange
+			protocol.Setup(p => p.GetParameter(Parameter.statuscodelatestlistings_3)).Returns("HTTP/1.1 200 OK");
+
+			var latestListing = new LatestListings
+			{
+				Status = new Status
+				{
+					ErrorCode = 0,
+					ErrorMessage = null,
+				},
+				Listings = new List<Listing>
+				{
+					new Listing
+					{
+						Id = null,
+						Name = "Bitcoin",
+						Symbol = "BTC",
+						DateAdded = DateTime.Parse("2010-07-13T00:00:00Z"),
+						CirculatingSupply = 19864978,
+						MaxSupply = 21000000,
+						CmcRank = 1,
+						Platform = null,
+						LastUpdated = DateTime.Parse("2025-10-01T07:37:21.5113403Z"),
+						Quote = new Quote
+								{
+									USD = new USD
+									{
+									Price = 101815.90673154943,
+									Volume24h = 45620279708.066856,
+									VolumeChange24h = -7.5884,
+									PercentChange1h = -0.4304306,
+									PercentChange24h = -1.83894047,
+									MarketCap = 2022570747272.2815,
+									MarketCapDominance = 61.8788,
+									},
+								},
+					},
+				},
+			};
+
+			string json = Newtonsoft.Json.JsonConvert.SerializeObject(latestListing);
+
+			protocol.Setup(p => p.GetParameter(Parameter.responselatestlistings_4)).Returns(json);
+
+			//Act
+
+			QAction.Run(protocol.Object);
+
+			//Assert
+
+			protocol.Verify(p => p.Log(It.IsAny<string>(), It.IsAny<LogType>(), It.IsAny<LogLevel>()));
+			protocol.Verify(p => p.FillArray(It.IsAny<int>(), It.IsAny<List<object[]>>(), NotifyProtocol.SaveOption.Full), Times.Never);
+		}
+	}
+}
