44d31998c7fafbef784526bed9a7255863c6b11a
TajanaSA <tajana.salkic@skyline.be>
2025-10-01 16:39:48 +0200

    Added tests for QAction_8 for Run method

---
+++ QAction_8Tests/QActionTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Tests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Moq;
+	using Skyline.DataMiner.Scripting;
+
+	[TestClass]
+	public class QActionTests
+	{
+		private static Mock<SLProtocol> protocol;
+
+		[TestInitialize]
+
+		public void TestInitialize()
+		{
+			protocol = new Mock<SLProtocol>();
+		}
+
+		[TestMethod]
+		public void RunTest_ShouldSetBearerToken_ValidAccessKeyToken()
+		{
+			string token = "bearer-token";
+			protocol.Setup(p => p.GetParameter(Parameter.acceskeytoken_312)).Returns(token);
+			string result = "Bearer " + token;
+
+			QAction.Run(protocol.Object);
+
+			protocol.Verify(p => p.SetParameter(It.IsAny<int>(), result), Times.Once);
+		}
+	}
+}
