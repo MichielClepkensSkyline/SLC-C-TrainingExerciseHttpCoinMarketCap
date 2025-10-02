d330ec0ba1c7958c258eb357b410d73d03ebc2e7
emirburic1 <emir.buric@skyline.be>
2025-10-02 10:59:52 +0200

    Added more unit tests for QAction7

--- QAction_7Tests/QActionTests.cs
+++ QAction_7Tests/QActionTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using QAction_1;
 using Skyline.DataMiner.Scripting;
+using Skyline.Protocol.MyExtension;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +17,7 @@ namespace Tests
         [TestMethod()]
         public void GetCategoryRowTest()
         {
-            //Arrange
+            // Arrange
             Mock<SLProtocol> mockProtocol = new Mock<SLProtocol>();
 
             mockProtocol.Setup(p => p.GetParameter(Parameter.httpresponsecategoryrefresh_518)).Returns("HTTP/1.1 200 OK");
@@ -72,13 +74,112 @@ namespace Tests
 
             }.ToObjectArray();
 
-            //Act
+            // Act
             QAction.GetCategoryRow(mockProtocol.Object);
 
-            //Assert
+            // Assert
             Assert.AreEqual(Parameter.Categories.tablePid, categoriesTablePid);
             Assert.AreEqual(expectedPrimaryKey, primaryKey);
             CollectionAssert.AreEqual(expectedRow, capturedRow, "SetRow row data mismatch.");
+        }
+
+        [TestMethod]
+        public void GetCategoryRow_InvalidHttpStatus_DoesNotCallFillArray()
+        {
+            // Arrange
+            var mockProtocol = new Mock<SLProtocol>();
+            var helperMock = new Mock<HelperMethods>();
+            string httpResponse = "HTTP/1.1 400 Bad Request";
+
+            mockProtocol.Setup(p => p.GetParameter(Parameter.httpresponsecategoryrefresh_518)).Returns(httpResponse);
+            helperMock.Setup(h => h.CheckStatusCode(httpResponse, mockProtocol.Object)).Returns(false);
+
+            // Act
+            QAction.GetCategoryRow(mockProtocol.Object);
+
+            // Assert
+            mockProtocol.Verify(p => p.FillArray(It.IsAny<int>(), It.IsAny<List<object[]>>(), It.IsAny<NotifyProtocol.SaveOption>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void GetCategoryRow_InvalidJsonStatus_DoesNotCallFillArray()
+        {
+            // Arrange
+            var mockProtocol = new Mock<SLProtocol>();
+            var helperMock = new Mock<HelperMethods>();
+            string httpResponse = "HTTP/1.1 200 OK";
+
+            mockProtocol.Setup(p => p.GetParameter(Parameter.httpresponsecategoryrefresh_518)).Returns(httpResponse);
+
+            string invalidJson = @"
+            {
+                ""status"": {
+                    ""timestamp"": ""2025-10-01T11:29:27.9980015Z"",
+                    ""error_code"": 1,
+                    ""error_message"": ""Not good"",
+                    ""elapsed"": 6,
+                    ""credit_count"": 1,
+                    ""notice"": null
+                    },
+                ""data"": 
+                 {
+                    ""id"": ""6823f463f4035758156a501c"",
+                    ""name"": ""Internet Capital Markets"",
+                    ""title"": ""Internet Capital Markets"",
+                    ""description"": ""Internet Capital Markets"",
+                    ""num_tokens"": 8,
+                    ""avg_price_change"": -19.930721654598546,
+                    ""market_cap"": 319300362.08430588,
+                    ""market_cap_change"": -18.342860046142,
+                    ""volume"": 490986489.37452543,
+                    ""volume_change"": -0.565025088224,
+                    ""last_updated"": ""2025-10-01T10:29:27.9980015Z""
+                }
+            }";
+            mockProtocol.Setup(p => p.GetParameter(Parameter.jsonrsponserefreshcategory_517)).Returns(invalidJson);
+
+            helperMock.Setup(h => h.CheckStatusCode(httpResponse, mockProtocol.Object)).Returns(true);
+            helperMock.Setup(h => h.CheckJSONResponseStatus(It.IsAny<Status>(), mockProtocol.Object)).Returns(false);
+
+            // Act
+            QAction.GetCategoryRow(mockProtocol.Object);
+
+            // Assert
+            mockProtocol.Verify(p => p.FillArray(It.IsAny<int>(), It.IsAny<List<object[]>>(), It.IsAny<NotifyProtocol.SaveOption>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void GetCategoryRow_InvalidData_DoesNotCallFillArray()
+        {
+            // Arrange
+            var mockProtocol = new Mock<SLProtocol>();
+            var helperMock = new Mock<HelperMethods>();
+            string httpResponse = "HTTP/1.1 200 OK";
+
+            mockProtocol.Setup(p => p.GetParameter(Parameter.httpresponsecategoryrefresh_518)).Returns(httpResponse);
+
+            string invalidJson = @"{
+                ""status"": {
+                    ""timestamp"": ""2025-10-01T12:00:00Z"",
+                    ""error_code"": 0,
+                    ""error_message"": null,
+                    ""elapsed"": 10,
+                    ""credit_count"": 1,
+                    ""notice"": null
+                },
+                ""data"": {}
+            }";
+
+            mockProtocol.Setup(p => p.GetParameter(Parameter.jsonrsponserefreshcategory_517)).Returns(invalidJson);
+
+            helperMock.Setup(h => h.CheckStatusCode(httpResponse, mockProtocol.Object)).Returns(true);
+            helperMock.Setup(h => h.CheckJSONResponseStatus(It.IsAny<Status>(), mockProtocol.Object)).Returns(false);
+
+            // Act
+            QAction.GetCategoryRow(mockProtocol.Object);
+
+            // Assert
+            mockProtocol.Verify(p => p.FillArray(It.IsAny<int>(), It.IsAny<List<object[]>>(), It.IsAny<NotifyProtocol.SaveOption>()), Times.Never);
         }
     }
 }
