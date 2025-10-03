2f64363238589c757278ebfe675d1f1a3eb0d1b1
sofiankerkhof <sofian.kerkhof@skyline.be>
2025-10-03 16:29:39 +0200

    Added regex to check status code.

--- QAction_1/HTTP/StatusCode.cs
+++ QAction_1/HTTP/StatusCode.cs
@@ -2,14 +2,16 @@
 namespace Skyline.DataMiner.Scripting.HTTP
 {
     using System;
+    using System.Text.RegularExpressions;
 
     public static class StatusCode
     {
         public static bool CheckStatusCode(SLProtocol protocol, int statusId, int contentId, int urlId)
         {
-            string statusLine = (string) protocol.GetParameter(statusId);
-            int statusCode = Int32.Parse(statusLine.Split(' ')[1]);
-            if (statusCode == 200)
+            string statusLine = (string)protocol.GetParameter(statusId);
+            var match = Regex.Match(statusLine, @"^HTTP\/\d\.\d\s+200(\s|$)");
+
+            if (match.Success)
             {
                 return true;
             }
