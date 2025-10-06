e27a6db3c46eb5afc056cfa2874fa811ea149058
sofiankerkhof <sofian.kerkhof@skyline.be>
2025-10-06 16:19:10 +0200

    Sanity check before splitting GetParameters response.

--- QAction_1/HTTP/StatusCode.cs
+++ QAction_1/HTTP/StatusCode.cs
@@ -2,6 +2,7 @@
 namespace Skyline.DataMiner.Scripting.HTTP
 {
     using System;
+    using System.Security.Policy;
     using System.Text.RegularExpressions;
 
     public static class StatusCode
@@ -10,19 +11,26 @@ namespace Skyline.DataMiner.Scripting.HTTP
         {
             object[] parameters = (object[]) protocol.GetParameters(new uint[] { statusCodeId, responseContentId, urlId });
 
-            string statusCode = (string)parameters[0];
-            string responseContent = (string)parameters[1];
-            string url = (string)parameters[2];
-
-            var match = Regex.Match(statusCode, @"^HTTP\/\d\.\d\s+200(\s|$)");
-
-            if (match.Success)
+            if (parameters.Length == 3)
             {
-                return true;
+                string statusCode = (string)parameters[0];
+                string responseContent = (string)parameters[1];
+                string url = (string)parameters[2];
+                var match = Regex.Match(statusCode, @"^HTTP\/\d\.\d\s+200(\s|$)");
+
+                if (match.Success)
+                {
+                    return true;
+                }
+                else
+                {
+                    protocol.Log($"QA{protocol.QActionID}|CheckStatusCode|Bad statuscode:\nURL API call: {url} \nStatuscode: {statusCode}\nResponse content: {responseContent}", LogType.Error, LogLevel.NoLogging);
+                    return false;
+                }
             }
             else
             {
-                protocol.Log($"QA{protocol.QActionID}|CheckStatusCode|Bad statuscode:\nURL API call: {url} \nStatuscode: {statusCode}\nResponse content: {responseContent}", LogType.Error, LogLevel.NoLogging);
+                protocol.Log($"QA{protocol.QActionID}|CheckStatusCode|GetParameters did not return the correct amount of params", LogType.Error, LogLevel.NoLogging);
                 return false;
             }
         }
